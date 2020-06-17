e93a64edf85a758f8cb167479ed6c08dbe5e3019
samahd626 <samahd626@gmail.com>
2020-06-17 12:29:41 +0100

    redesigned the home page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once "../navbar/nav.php"  ?>
+<?php include_once "navbar/nav.php"  ?>
 
 
 
@@ -9,12 +9,15 @@
 	<meta name="description" content="some stuffs">
 	<meta name="keywords" content="grant, scholarship, eon">
 	<meta name="author" content="just samahd">
+	<link href="favicon.ico" rel="icon" type="image/x-icon">
+   
+    <link rel="apple-touch-icon" type="image/x-icon" href = "favicon.ico"/>
 	<meta name="viewport" content="width=device-width, initial-scale = 1.0">
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" />
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css">
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="styles.css" />
-	<link rel="stylesheet" type="text/css" href="../navbar/nav.css">
+	<link rel="stylesheet" type="text/css" href="navbar/nav.css">
 
 
 	<title>EON | Grant and scholarship</title>
@@ -23,9 +26,9 @@
 
 <div class="introsec" id="scholarship">
 	<h1 class="joinsch animated bounceInDown">Join the EON scholarship program today!</h1>
-	<div class="joinschmoreinfo animated bounceInUp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis lacinia est. Nulla malesuada massa orci,<br> vitae placerat lorem pharetra a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> Vestibulum quis lacinia est. </div>
+	<div class="joinschmoreinfo animated bounceInUp">Education Online Nigeria Grant and Scholarship platform. Perfect place to gain a scholarship <br>or generate fund for your project through Grant. </div>
 	<span class="animated bounce"></span>
-	<br /><br />
+	<br /><br /><br />
 	<div class="schtype row">
 
 		<div class="col-md-4">
@@ -58,7 +61,8 @@
 	</div>
 </div><br>
 
-<div class="row scroll-animations">
+<br>
+<div class="row scroll-animations hide">
 	<div class="col-md-6">
 		<div class="alreadyknw animated row">
 			<span class="col-md-3"><img src="pig.svg" /></span> 
@@ -68,7 +72,7 @@
 				</div>
 				<div>
 				Finance your project through crowd funding or competition on our platform.
-				<br /><div class="align-right"><div class="btn btn-success"><a href="reg.php"> Register</a></div></div>
+				<br /><div class="align-right"><div class="btn btn-success"><a href="register/register.php"> Register</a></div></div>
 				</div>
 			</div>
 	</div>
@@ -94,30 +98,31 @@
 <div class="row ">
 	<div class="col-md-8 whyeon">
 		<h2>Prepare for your scholarship exam with our E-books</h2><br>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis lacinia est. Nulla malesuada massa orci, vitae placerat lorem pharetra a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis lacinia est. Nulla malesuada massa orci, vitae placerat lorem pharetra a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis lacinia est. Nulla malesuada massa orci, vitae placerat lorem pharetra a. 
-			<div class="align-right start-prep"><div class="btn btn-success"><a href="../register/register.php">Start Prep today</a></div></div>
+		<p>With hundreds of books on your preferred course at <a href="#">EON E-library</a> written by proffessionals in such field, study for our scholarship exam and stand a greater chance to become our next beneficiary. 
+			<div class="align-right start-prep"><div class="btn btn-success"><a href="register/register.php">Start Prep today</a></div></div>
   </p>
 	</div>
-	<div class="col-md-4 cardcol scroll-animations">
+	<div class="col-md-4 cardcol scroll-animations hide">
 		<div class="cardi animated">
 			<h4>Why EON Grant/Scholarship?</h4>
 			<ul>
-				<li> consectetur adipiscing elit. Vestibulum quis lacinia est. Nulla malesuada massa orci, vitae placerat lorem pharetra a</li>
-				<li> consectetur adipiscing elit. Vestibulum quis lacinia est. Nulla malesuada massa orci, vitae placerat lorem pharetra a</li>
-				<li> consectetur adipiscing elit. Vestibulum quis lacinia est. Nulla malesuada massa orci, vitae placerat lorem pharetra a</li>
-				<li> consectetur adipiscing elit. Vestibulum quis lacinia est. Nulla malesuada massa orci, vitae placerat lorem pharetra a</li>
+				<li> EON deals basically with local content which provides oppotunities to Nigerians</li>
+				<li> EON provides best e-books so as to help candidates prepare.</li>
+				<li> We grant scholarship for any course to any Institution of your choice in Nigeria</li>
+				
 			</ul>
 
 		</div>
 		
 	</div>
 </div>
+<br><hr><br><br>
 
 
-
-<div class="grantheader" id="grant">
+<div class="grantheader bg-primary" id="grant">
 	
 		<div class="apysch">
+
 			<h1 class="grantccontent">Apply for Grant!</h1>
 			<br />
 		
@@ -128,26 +133,37 @@
 <div class="row smallboxcontainer">
 				<div class=" col-md-6 smallboxtext">
 					<h1>Request Grant for your Agricultural project!</h1>
+					<div class="moreinfo">
+						Get a non-repayable fund disbursed to you for your agro project.
+					</div>
+					<img src="barley.png">
 				</div>
 				<div class=" col-md-6 smallbox">
-					<img src="tractor.jpg">
+					<img src="tractor.svg">
 				</div>
 				
 			</div>
-<br />
+
 
 			<div class="row smallboxcontainer">
 				<div class=" col-md-6 smallbox">
-					<img src="yoga.jpg" height="450">
+					<img src="footergraphics.svg" height="450">
 				</div>
 				<div class=" col-md-6 smallboxtext">
 					<h1>Request Grant for your School project project!</h1>
+					<div class="moreinfo">
+						Get a non-repayable fund disbursed to you for your agro project.
+					</div>
+					<img src="finance.svg">
+
 				</div>
+
 				
 				
 			</div>
 			<br /><br />
-		<footer class="bg-dark">
+			<div class="footercontainer">
+		<footer >
 			<div class="row">
 				
 				<div class="col-md-3">
@@ -157,11 +173,7 @@
 						<div class="footerlist"><a href="register.php" >-Learn about this service</a></div>
 						<div class="footerlist"><a href="register.php" >-Talk to us</a></div>
 						<div class="footerlist"><a href="register.php" >-Terms and conditions</a></div>
-					
-					
-					
-				</div>
-				<div class="col-md-3">
+					<br><br>
 					<h5>Scholarship</h5>
 
 
@@ -170,10 +182,9 @@
 						<div class="footerlist"><a href="register.php" >-Learn more about this service</a></div>
 						<div class="footerlist"><a href="register.php" >-Talk to us</a></div>
 						<div class="footerlist"><a href="register.php" >-Terms and conditions</a></div>
-					
-					
-
+								
 				</div>
+				
 				<div class="col-md-3">
 					<h5>Other Services</h5>
 
@@ -182,30 +193,46 @@
 						<div class="footerlist"><a href="register.php" >-EON Certifications and Examination</a></div>
 						<div class="footerlist"><a href="register.php" >-Career Guilding and Councelling</a></div>
 						<div class="footerlist"><a href="register.php" >-Crowd Funding</a></div>
+						<br>
 					
-
-				</div>
-				<div class="col-md-3">
-					<h5>information</h5>
+					<h5>Information</h5>
 
 						
 						<div class="footerlist"><a href="register.php" >-About us</a></div>
 						<div class="footerlist"><a href="register.php" >-Talk to us</a></div>
 						<div class="footerlist"><a href="register.php" >-General Terms and conditions</a></div>
 						<div class="footerlist"><a href="register.php" >-FAQ</a></div>
+
+				</div>
+				<div class="col-md-6">
+					
+					<div class="contacts" align="left">
+						<span><i class="fa fa-map-marker"></i> Kwara State Library Ilorin, Nigeria.</span>
+						&nbsp;
+						&nbsp;
+						&nbsp;
+						<span><i class="fa fa-phone"></i> +2348136486870</span><br /><br />
+						<span><i class="fa fa-paper-plane"></i> hello@eon.com.ng </span>
+					</div><br /><br />
+					<div class="subscribe">
+						<h5>Subscribe to EON newsletter.</h5>
+						<span class="footerlist" style="font-weight: lighter;">Get the latest EON news, products, and offers sent directly to your inbox</span><br/><br/>
+						<input type="text" name="subscribeemail" class="form-control sub" placeholder="Enter your email">
+						<input type="submit" name="subscribebtn" class="btn btn-success float-right" value="Subscribe">
+					</div>
 					
 
 				</div>
 			</div>
 		</footer>
-		<div class="copyright foat-left">&copy; Education Online Nigeria | 2020
-			<span class="social float-right">
+		<b>&copy; Education Online Nigeria | 2020</b>
+			
+		<span class="social" style="float: right; padding-right:10px;">
 				<i class="fa fa-facebook"></i>
 				<i class="fa fa-twitter"></i>
 				<i class="fa fa-linkedin"></i>
 			</span>
-		</div>
-
+</div>
 </body>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
--- navbar/nav.php
+++ navbar/nav.php
@@ -1,9 +1,14 @@
-
-	<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+  <head>
+    <link href="favicon.ico" rel="icon" type="image/x-icon">
+   
+    <link rel="apple-touch-icon" type="image/x-icon" href = "favicon.ico"/>
+  </head>
+	
+  <nav class="navbar navbar-expand-lg navbar-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">EON</a>
+  <a class="navbar-brand" href="#"><img src="logo.jpg" /></a>
 
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
